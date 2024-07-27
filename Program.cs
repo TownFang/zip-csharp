fa2d75cc1a1c959663d5653b2ee516ef64cdb570
fangziping <768449566@qq.com>
2024-07-27 18:08:51 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("我的第一个程序");
+// Console.ReadKey();
+
+// 类型
+class Program
+{
+    // 函数 方法
+    static void Main()
+    {
+        var a = 10;
+        var b = 10;
+        var c = a + b;
+
+
+    }
+}
